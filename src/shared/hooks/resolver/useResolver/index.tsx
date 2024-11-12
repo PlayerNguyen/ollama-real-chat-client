21fc4981ad8ff1ec371bd2dea08bc1b987d78e6a
PlayerNguyen <nhnguyen.forwork@gmail.com>
2024-11-13 00:17:09 +0700

    fix(resolver): the resolver cannot parse json if the response have more than one body

--- src/shared/hooks/resolver/useResolver/index.tsx
+++ src/shared/hooks/resolver/useResolver/index.tsx
@@ -4,8 +4,12 @@ export default function useResolver<T>() {
   async function tick(input: Uint8Array) {
     const decodedString = decoder.decode(input);
     console.log(decodedString);
+    const result = decodedString
+      .split("\n")
+      .filter((v) => v !== "")
+      .map((v) => JSON.parse(v));
 
-    return JSON.parse(decoder.decode(input)) as T;
+    return result as T[];
   }
 
   return { tick };
